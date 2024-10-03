ab51c72a8e827cce9c452e92320ae8cace139632
Merlin <30577208+SimplyMerlin@users.noreply.github.com>
2024-10-03 21:26:00 +0200

    oops

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import rehypeExternalLinks from "rehype-external-links";
 import { remarkReadingTime } from "./remark-reading-time.ts";
-import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
@@ -26,6 +23,11 @@ export default defineConfig({
       ],
     ],
   },
+
   site: "https://simplymerlin.com",
   integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
