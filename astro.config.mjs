05ab2eb1a5bf17a6c226ae59495edcabd1603ad5
Merlin <30577208+SimplyMerlin@users.noreply.github.com>
2024-10-03 21:22:10 +0200

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import rehypeExternalLinks from "rehype-external-links";
 import { remarkReadingTime } from "./remark-reading-time.ts";
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
