deeb56f01eb7ccc805c83cbd0ff96561bb17343e
Merlin <30577208+SimplyMerlin@users.noreply.github.com>
2024-10-04 19:56:10 +0200

    finally add prettier lol

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SimplyMerlin';
-export const SITE_DESCRIPTION = 'Horrible site';
+export const SITE_TITLE = "SimplyMerlin";
+export const SITE_DESCRIPTION = "Horrible site";
